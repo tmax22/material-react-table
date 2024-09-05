e6d27acb91c722695d44308be63488cdc93b8ea7
superodde <superodde@gmail.com>
2024-09-05 02:59:51 +0200

    Update no.ts (#1233)
    Fixed placeholders for info regarding selected rows

--- packages/material-react-table/src/locales/no.ts
+++ packages/material-react-table/src/locales/no.ts
@@ -93,5 +93,5 @@ export const MRT_Localization_NO: MRT_Localization = {
   unpinAll: 'Løsne alle',
 
   ungroupByColumn: 'Oppgrupper etter {column}',
-  selectedCountOfRowCountRowsSelected: '{selected} av {count} rader valgt',
+  selectedCountOfRowCountRowsSelected: '{selectedCount} av {rowCount} rader valgt',
 };
